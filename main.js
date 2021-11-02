45833644ec20c1f7b80094f5cd1545e95f1e7a1d
ElasticBow <79611589+ElasticBow@users.noreply.github.com>
2021-11-02 15:52:02 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -15,12 +19,19 @@ function modelLoaded(){
 
 function draw() {
     background('#969A97');
+    fill('#F90093');
+    text("Kenneth",100,350)
+    textSize(difference);
 }
 
 function gotPoses(results)
 {
     if(results.length > 0)
     {
-        console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
     }
 }
